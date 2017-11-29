70d495aa793de4cd3e8cfafa222577aad9d8ef18
GUOJINGWU <gjwxs@foxmail.com>
2017-11-29 23:28:20 +0800

    vue项目第一天，结构搭建，主页面，新闻详情和评论完成

--- src/routers/router.js
+++ src/routers/router.js
@@ -5,11 +5,47 @@ import Vue from "vue"
 //使用Vue.use方法加载VueRouter模块
 Vue.use(VueRouter)
 
-import HelloVue from "../components/HelloVue.vue"
+//引入组件 tab栏相关的组件
+import HomeContainer from "../components/tabcontainers/HomeContainer.vue"
+import MemberContainer from "../components/tabcontainers/MemberContainer.vue"
+import CartContainer from "../components/tabcontainers/CartContainer.vue"
+import SearchContainer from "../components/tabcontainers/SearchContainer.vue"
+
+
+//新闻组件
+import NewsInfo from "../components/news/NewsInfo.vue"
+import NewsList from "../components/news/NewsList.vue"
 
 export default new VueRouter({
     routes: [
-       {path: "/",  component: HelloVue}
+        {
+            path: "/home",
+            component: HomeContainer
+        },
+        {
+            path: "/member",
+            component: MemberContainer
+        },
+        {
+            path: "/cart",
+            component: CartContainer
+        },
+        {
+            path: "/search",
+            component: SearchContainer
+        },
+        {
+            path: "/",
+            redirect: "/home"
+        },
+        {
+            path: "/home/newslist",
+            component: NewsList
+        },
+        {
+            path: "/home/newsinfo/:id",
+            component: NewsInfo
+        }
     ]
 });
 
